8bc0fbd74f684de153cad448fe8f23830efbae4d
gnguser <gng@3ds.com>
2017-03-07 23:27:59 +0000

    Added conflict line from gnguser

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	    System.out.println("Hello World!");
 	    System.out.println("This is from 'bruceschu'");
 	    System.out.println("This is from 'gnguser'");
+	    System.out.println("next line from gnguser");
     }
 }
