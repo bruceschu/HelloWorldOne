77c31510eee17a51354739f8b08b77a211abf324
gnguser <gng@3ds.com>
2017-03-08 16:07:48 +0000

    merge03 gnguser

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ public class Main {
 	    System.out.println("next line from gnguser and bruceschu");
 	    System.out.println("merge01 gnguser and bruceschu");
 	    System.out.println("merge02 bruceschu and gnguser");
+	    System.out.println("merge03 gnguser");
     }
 }
