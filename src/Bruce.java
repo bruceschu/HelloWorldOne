d61266b28d969d4c6576a0fdd5af7616e3a11c0f
gnguser <gng@3ds.com>
2017-03-08 18:08:42 +0000

    merge01 gnguser

--- src/Bruce.java
+++ src/Bruce.java
@@ -4,5 +4,6 @@ public class Bruce {
     public static void main(String[] args) {
 	    // write your code here
 	    System.out.println("Bruce init from bruceschu");
+	    System.out.println("merge01 gnguser");
     }
 }
