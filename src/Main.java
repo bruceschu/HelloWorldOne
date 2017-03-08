de0ef4f22f228e072d11ea799a9d91035027f13a
bruceschu <xaustie@gmail.com>
2017-03-08 13:57:12 +0000

    This is middle from bruceschu

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	    System.out.println("Hello World!");
 	    System.out.println("This is from 'bruceschu'");
 	    System.out.println("This is from 'gnguser'");
+	    System.out.println("middle line by bruceschu");
 	    System.out.println("next line from gnguser and bruceschu");
     }
 }
