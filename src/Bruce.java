d81f96f4b5bba7af38440c0dae0ad4419486bfb5
Bruce SCHU <xaustie@gmail.com>
2017-03-08 22:33:38 +0000

    simple02 bruceschu

--- src/Bruce.java
+++ src/Bruce.java
@@ -6,6 +6,7 @@ public class Bruce {
 	    System.out.println("Bruce init from bruceschu");
 	    System.out.println("merge01 bruceschu and gnguser");
 	    System.out.println("simple01 line add bruceschu");
+	    System.out.println("simple02 line add bruceschu");
 	    System.out.println("merge02 bruceschu and gnguser");
 	    System.out.println("simple01 gnguser");
     }
